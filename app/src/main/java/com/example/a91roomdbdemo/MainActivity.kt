c92f2b6097cc5072fa0cf0fde91991168ff3d9d6
VictorChuah <68944095+VictorChuah@users.noreply.github.com>
2021-08-25 13:58:03 +0800

    Week 10 DB CRUD

--- app/src/main/java/com/example/a91roomdbdemo/MainActivity.kt
+++ app/src/main/java/com/example/a91roomdbdemo/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.a91roomdbdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.room.Dao
 import com.example.a91roomdbdemo.data.Product
 import com.example.a91roomdbdemo.data.ProductDB
 import com.example.a91roomdbdemo.data.ProductDao
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var dao :ProductDao
+    lateinit var dao :ProductDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +22,37 @@ class MainActivity : AppCompatActivity() {
 
         dao = ProductDB.getInstance(application).productDao
 
-        val btn :Button = findViewById(R.id.btnInsert)
+        val btnInsert :Button = findViewById(R.id.btnInsert)
+        btnInsert.setOnClickListener(){
 
-        btn.setOnClickListener(){
-            val p = Product(0, "Kang", 1.10)
+            val name :String  = findViewById<TextView>(R.id.tfName).text.toString()
+            val price:Double =  findViewById<TextView>(R.id.tfPrice).text.toString().toDouble()
+
+            val p = Product(0, name, price)
 
             CoroutineScope(IO).launch {
                 dao.insertProduct(p)
             }
 
+        }
+
+        val btnGet :Button = findViewById(R.id.btnGet)
+        btnGet.setOnClickListener(){
+
+            CoroutineScope(IO).launch {
+                var productName = ""
+                //val products /*:List<Product>*/ = dao.getAll()
+                val products :List<Product> = dao.getPriceLessThan(5.00)
+
+                for (product: Product in products){
+                    productName += product.name + "\n"
+                }
+
+                CoroutineScope(Main).launch {
+                    val tvResult: TextView = findViewById(R.id.tvResult)
+                    tvResult.text = productName
+                }
+            }
 
         }
     }
--- app/src/main/java/com/example/a91roomdbdemo/data/ProductDao.kt
+++ app/src/main/java/com/example/a91roomdbdemo/data/ProductDao.kt
@@ -2,9 +2,16 @@ package com.example.a91roomdbdemo.data
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface ProductDao {
     @Insert
     fun insertProduct(p:Product)
+
+    @Query ("SELECT * FROM product_table")
+    fun getAll() : List<Product> //array also can
+
+    @Query("SELECT * FROM product_table WHERE price < :price")
+    fun getPriceLessThan(price :Double) :List<Product>
 }
